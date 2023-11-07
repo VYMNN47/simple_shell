b44a17c9a5baaeccc004d08354bbe6ab208b9f10
Ahmed <ahmed_l2@yahoo.com>
2023-11-08 01:10:10 +0300

    updated

--- 10-builtin_executables.c
+++ 10-builtin_executables.c
@@ -5,3 +5,17 @@ void exec_exit(char **cmd, int status)
 	free2d(cmd);
 	exit(status);
 }
+
+void exec_env(char **cmd, int status)
+{
+	int index;
+	(void)status;
+
+	for (index = 0; environ[index] != NULL; index++)
+	{
+		write(STDOUT_FILENO, environ[index], _strlen(environ[index]));
+		write(STDOUT_FILENO, "\n", 1);
+	}
+
+	free2d(cmd);
+}
