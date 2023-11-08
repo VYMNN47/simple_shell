3e33b045b16273a7fc04f31fde5a928c9e616dc2
aymnn <ayman.belahsen.4@gmail.com>
2023-11-08 10:55:50 -0800

    Adding description and comments

--- 10-builtin_executables.c
+++ 10-builtin_executables.c
@@ -1,10 +1,27 @@
 #include "shell.h"
 
+/**
+ * exec_exit - Execute the built-in "exit" command
+ * @cmd: An array of command arguments (unused)
+ * @status: The exit status of the shell program
+ *
+ * This function frees allocated memory and exits the shell program with the
+ * specified status code.
+ */
+
 void exec_exit(char **cmd, int status)
 {
-	free2d(cmd);
-	exit(status);
+	free2d(cmd);/* Free the allocated memory for command arguments */
+	exit(status);/* Terminate the shell program with the given exit status */
 }
+
+/**
+ * exec_env - Execute the built-in "env" command
+ * @cmd: An array of command arguments (unused)
+ * @status: The exit status of the shell program (unused)
+ *
+ * This function prints the environment variables to the standard output.
+ */
 
 void exec_env(char **cmd, int status)
 {
@@ -13,8 +30,8 @@ void exec_env(char **cmd, int status)
 
 	for (index = 0; environ[index] != NULL; index++)
 	{
-		write(STDOUT_FILENO, environ[index], _strlen(environ[index]));
-		write(STDOUT_FILENO, "\n", 1);
+		write(STDOUT_FILENO, environ[index], _strlen(environ[index]));/* Write each environment variable to the standard output */
+		write(STDOUT_FILENO, "\n", 1);/* Write a newline character for separation */
 	}
 
 	free2d(cmd);
